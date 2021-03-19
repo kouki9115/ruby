306c07f8a5ff3b65f713d8f90316d9720042fc5c
kouki9115 <kouki.n9115@gmail.com>
2021-03-19 09:39:54 +0000

    Start3/19

--- Fizz.rb
+++ Fizz.rb
@@ -10,3 +10,16 @@
   puts n
   end
 end
+
+(1..30).each do |n|
+  
+  if n % 15 == 0
+  puts "FizzBuzz"
+  elsif n % 3 == 0
+  puts "Fizz"
+  elsif n % 5 == 0
+  puts "Buzz"
+  else
+  puts n
+  end
+end
